10cc3e3f33bd648860d370c8214f819d351293e2
UMuhitdinov <usmon1983@gmail.com>
2020-09-25 10:38:06 +0500

    v2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/usmon1983/bank
+module github.com/usmon1983/bank/v2
 
 go 1.15
--- pkg/types/types.go
+++ pkg/types/types.go
@@ -4,8 +4,17 @@ type Money int64
 
 type Category string
 
+type Status string
+
+const (
+	StatusOk Status = "OK"
+	StatusFail Status = "FAIL"
+	StatusInProgress Status = "INPROGRESS"
+)
+
 type Payment struct {
 	ID int
 	Amount Money
 	Category Category
+	Status Status
 }
